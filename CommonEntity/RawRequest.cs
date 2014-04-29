6e83c13726d1514735a051ef515d558be6648db6
felinegrace <felinegrace@163.com>
2014-04-29 16:45:30 +0800

    before rebuild namespace

---
+++ CommonEntity/RawRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cabinet.CommonEntity
+{
+    public class RawRequest : Jsonable
+    {
+        public string method { get; set; }
+        public List<object> param { get; private set; }
+        public RawRequest()
+        {
+            param = new List<object>();
+        }
+    }
+}
